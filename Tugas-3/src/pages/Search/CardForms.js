256cc2a5e8f4707b060b0a082c1189b630311417
CaamVilvactDJavu <caam.vilvact@gmail.com>
2022-10-22 15:23:05 +0700

    Tugas-3

---
+++ Tugas-3/src/pages/Search/CardForms.js
@@ -0,0 +1,70 @@
+import { Component } from "react";
+import { StyleSheet, Button, View } from "react-native";
+import FormTextInput from "./FormTextInput.js";
+
+class CardForms extends Component {
+  search() {
+    this.props.navigation.navigate("SearchResult");
+  }
+  render() {
+    return (
+      <View style={styles.wrapper}>
+        <View style={styles.card}>
+          <View style={[styles.mb3]}>
+            <FormTextInput
+              label="Lokasi Keberangkatan"
+              placeholder="Masukkan lokasi keberangkatan"
+              icon="plane-departure"
+            />
+          </View>
+          <View style={[styles.mb3]}>
+            <FormTextInput
+              label="Lokasi Tujuan"
+              placeholder="Masukan lokasi tujuan"
+              icon="plane-arrival"
+            />
+          </View>
+          <View style={[styles.mb3]}>
+            <FormTextInput
+              label="Tanggal Keberangkatan"
+              placeholder="Masukan tanggal keberangkatan"
+              icon="calendar"
+            />
+          </View>
+          <View style={[styles.mb3]}>
+            <Button
+              title="CARI"
+              color="#E47D24"
+              onPress={() => this.search()}
+            />
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.creat({
+  wrapper: {
+    paddingHorizontal: 34,
+    marginTop: 88,
+  },
+  mb3: {
+    marginBottom: 18,
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 18,
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 3,
+      height: 3,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 8,
+    elevation: 4,
+  },
+});
+
+export default CardForms;
